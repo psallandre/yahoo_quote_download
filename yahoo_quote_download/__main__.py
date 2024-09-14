8716d9bb3a3990f641f9ed6cebcccdfdc3b278ed
psallandre <psallandre@gmail.com>
2024-09-14 11:29:50 +0200

    Put back '--begindate' parameter

--- yahoo_quote_download/__main__.py
+++ yahoo_quote_download/__main__.py
@@ -10,6 +10,7 @@ def main():
     x = p.add_mutually_exclusive_group()
     x.add_argument('-d', '--days', type=int, default=1, help='Number of days of results to return (default %(default)s)')
     x.add_argument('-L', '--latest', action='store_true', help='Just return the one most-recent row for each ticker')
+    x.add_argument('-b', '--begindate', help='The beginning date (YYYY-MM-DD)')
     p.add_argument('-t', '--tsv', action='store_const', const='\t', dest='sep', default=',',
                    help='Output TSV (tab-separated) rather than CSV (comma-separated)')
     p.add_argument('ticker', nargs='+')
@@ -28,6 +29,8 @@ def main():
 
     if args.latest:
         begindate, max_rows = None, 1
+    elif args.begindate:
+        max_rows, begindate = None, int(datetime.strptime(args.begindate, '%Y-%m-%d').timestamp())
     else:
         max_rows, begindate = None, int(time.time()-86400*args.days)
     sys.stdout.writelines( yq.csv(args.ticker, events=args.events, headers=args.header, begindate=begindate, max_rows=max_rows, sep=args.sep) )
